b94606fe01b2833d06e995a7235d24e268c32585
Allen <thinhnv16@fpt.com>
2023-12-07 12:56:28 +0700

    add service config object

--- src/main/java/com/example/demo/service/FakeDataService.java
+++ src/main/java/com/example/demo/service/FakeDataService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 
 public interface FakeDataService {
-    String getData(String group, String name);
+    Object getData(String group, String name);
 
     void addData(String group, String name, Object data);
 }
